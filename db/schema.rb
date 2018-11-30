304757674a8c6d8f8beb4feeb6ddc4f201280d32
Geoff Cheah <geoffcheah@gmail.com>
2018-11-30 14:59:14 -0800

    routes, controllers/ endpoints done for buttons
    List all buttons, create a button.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_11_30_214635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buttons", force: :cascade do |t|
+    t.string "text"
+    t.integer "colour_h"
+    t.integer "colour_s"
+    t.integer "colour_l"
+    t.integer "width"
+    t.integer "height"
+    t.integer "position"
+    t.integer "click_through_delta"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
