285fab8f8a451aaae702357091e053cd84e57208
David Cava <david.cava.pina@gmail.com>
2018-11-30 14:36:49 -0800

    preprocs

--- data-science/src/training_model.py
+++ data-science/src/training_model.py
@@ -1,8 +1,9 @@
 import os
-import tensorflow
+import tensorflow as tf
 from tensorflow.contrib.layers import dropout
 import numpy as np 
 import pandas as pd 
+import helpers as h
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -30,6 +31,8 @@ MODEL_NAME = "optimusai"
 MODEL_PATH = "trained_model"
 
 ## NN Params
+test_size = 0.3
+
 learning_rate = 0.001
 n_epochs = 2000
 keep_prob = 0.6
@@ -44,3 +47,30 @@ l4_nodes = 400
 
 tf.reset_default_graph()
 ## Graph definition
+
+### Input Layer
+with tf.variable_scope("inputs"):
+    X = tf.get_variable(tf.float32, shape=[None, n_inputs], name = 'input')
+
+with tf.variable_scope("layer1"):
+    weights = 
+    biases = 
+
+
+
+# PREPROCESSING
+
+raw_data = pd.read_csv("inputs/raw_data.csv", index_col = 0)
+in_scaler, out_scaler, le_txt = h.encoders(raw_data)
+
+checkpoint = tf.train.latest_checkpoint(model_path)
+should_train = checkpoint == None
+
+with tf.Session() as session:
+    if should_train:
+        print("Training")
+        saver = tf.train.Saver()
+
+        # Split training and testing
+        in_scaled, out_scaled = h.preprocess_training(raw_data, le_txt, in_scaler, out_scaler)
+        in_training, in_test, out_training, out_test = h.training_test(in_scaled, out_scaled, out_scaler, test_size)
