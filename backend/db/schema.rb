bf888ef748a965b08e032250d3b9990a1cc29f35
Geoff Cheah <geoffcheah@gmail.com>
2018-11-30 21:20:00 -0800

    change port number & migrate
    Change development port back to 5432 and run migrations.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_12_01_045931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buttons", force: :cascade do |t|
+    t.string "text"
+    t.integer "position"
+    t.integer "click_through_delta"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "mobile"
+    t.integer "age"
+    t.string "gender"
+    t.string "region"
+  end
 
 end
