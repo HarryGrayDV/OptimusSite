9d0fcd8fc28f7dd2749693e498712eb905b365e0
Geoff Cheah <geoffcheah@gmail.com>
2018-12-01 14:47:56 -0800

    Add hash to models
    This represents the name of the model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_215440) do
+ActiveRecord::Schema.define(version: 2018_12_01_224538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_12_01_215440) do
     t.datetime "updated_at"
     t.boolean "mobile"
     t.integer "ctd"
+    t.string "hash"
   end
 
 end
