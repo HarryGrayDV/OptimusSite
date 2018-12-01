f7d5fbe9731a9014e4da7323deb8124a532f2b54
David Cava <david.cava.pina@gmail.com>
2018-12-01 13:19:49 -0800

    NLP done

---
+++ data-science/nlp/topicmodel.py
@@ -0,0 +1,63 @@
+import csv
+import nltk
+nltk.download("stopwords")
+nltk.download("wordnet")
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+import string
+import gensim
+from gensim import corpora
+from gensim.models.coherencemodel import CoherenceModel
+
+# compile documents
+# Import and read the csv file
+ifile  = open('nlp/inputs.csv', "r")
+read = csv.reader(ifile)
+doc_complete = []
+for row in read :
+    doc_complete.append(row[0])
+
+# Remove stopwords, punctuation and normalize
+stopwords = stopwords.words('english')
+stopwords.append('lacroix')
+stop = set(stopwords)
+
+exclude = set(string.punctuation) 
+lemma = WordNetLemmatizer()
+def clean(doc):
+    stop_free = " ".join([i for i in doc.lower().split() if i not in stop])
+    punc_free = ''.join(ch for ch in stop_free if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
+    return normalized
+
+doc_clean = [clean(doc).split() for doc in doc_complete]  
+
+# Creating the term dictionary of our courpus, where every unique term is assigned an index.
+dictionary = corpora.Dictionary(doc_clean)
+
+# Converting list of documents (corpus) into Document Term Matrix using dictionary prepared above.
+doc_term_matrix = [dictionary.doc2bow(doc) for doc in doc_clean]
+
+# Creating the object for LDA model using gensim library
+Lda = gensim.models.ldamodel.LdaModel
+
+# Running and Trainign LDA model on the document term matrix.
+ldamodel = Lda(doc_term_matrix, num_topics=5, id2word = dictionary, passes=50)
+topics = ldamodel.print_topics(num_words=3)
+for topic in topics:
+    print(topic)
+
+# Compute Coherence Score
+coherence_model_lda = CoherenceModel(model=ldamodel, texts=doc_clean, dictionary=dictionary, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: {:0.2f}%'.format(coherence_lda*100))
+
+# Create a new corpus, made of previously unseen documents.
+other_texts = ['naturally water', 'just buy', 'things']
+clean_other = [clean(doc).split() for doc in other_texts]
+other_corpus = [dictionary.doc2bow(doc) for doc in clean_other]
+
+for doc in range(len(other_corpus)):
+    unseen_doc = other_corpus[doc]
+    vector = ldamodel[unseen_doc] # get topic probability distribution for a document
+    print(vector)
